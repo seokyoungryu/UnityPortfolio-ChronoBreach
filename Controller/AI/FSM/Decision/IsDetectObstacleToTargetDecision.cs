bb7dacbffd704d1b6fade08ccd8ac253cf590c3f
seokyoungryu <38558209+seokyoungryu@users.noreply.github.com>
2025-10-27 18:19:51 +0900

    Update IsDetectObstacleToTargetDecision.cs

--- Controller/AI/FSM/Decision/IsDetectObstacleToTargetDecision.cs
+++ Controller/AI/FSM/Decision/IsDetectObstacleToTargetDecision.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AI/Decisions/Is Detect Obstacle To Target", fileName ="IsDetectObstacleToTargetDecision")]
 public class IsDetectObstacleToTargetDecision : Decision
 {
-    //ai와 타겟 사이에 장애물이 검출되면 timer를 함. 
-    //특정 시간이 지나면 resetPosition으로.
-
-    //그리고 ResetPosition에서 만약 reset가능할 경우 바로 돌아간느게 아니라 변수의 waitTime만큼 그 자리에서 Find하다가 돌아가기??
 
     public override void OnInitDecide(AIController controller)
     {
